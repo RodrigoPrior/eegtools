413f937ebfc504c5035a93c11330b522a5a5164d
rodrigoprior <rprior@infranology.com.br>
2017-04-27 21:38:26 -0300

    fix print to py3 and docstring

--- examples/ex_csp_motor_imagery.py
+++ examples/ex_csp_motor_imagery.py
@@ -1,22 +1,24 @@
-import numpy as np
-from scipy import signal
-from sklearn import svm, pipeline, base, metrics
-import eegtools
+"""
+This example demonstrates the use of the common spatial patterns (CSP).
 
-'''
-This example demonstrates the use of the common spatial patterns (CSP)
-algorithm [1] on EEG data with motor imagery form BCI competition 3.4a.
+Algorithm [1] on EEG data with motor imagery form BCI competition 3.4a.
+
 For classification, scikit-learn (http://scikit-learn.org) is used.
 
 [1] Zoltan J. Koles. The quantitative extraction and topographic mapping
 of the abnormal components in the clinical EEG. Electroencephalography
 and Clinical Neurophysiology, 79(6):440--447, December 1991.
-'''
+"""
+import numpy as np
+from scipy import signal
+from sklearn import svm, pipeline, base, metrics
+import eegtools
+
 
 # Automatically download and load dataset from BCI competition 3. For
 # experimental info see: http://www.bbci.de/competition/iii/#data_set_iva .
 d = eegtools.data.bcicomp3_4a.load('aa')
-print d
+print(d)
 
 # create band-pass filter for the  8--30 Hz where the power change is expected
 (b, a) = signal.butter(3, np.array([8, 30]) / (d.sample_rate / 2), 'band')
@@ -26,8 +28,7 @@ d.X = signal.lfilter(b, a, d.X, 1)
 
 # extract trials
 offsets = np.array([0.5 * d.sample_rate, np.median(d.events[2] - d.events[1])])
-print 'Extracting interval of %s sec from trial start.' % \
-  (offsets / d.sample_rate)
+print('Extracting interval of %s sec from trial start.' % (offsets / d.sample_rate))
 trials, _ = eegtools.featex.windows(d.events[1], offsets, d.X)
 
 # extract labels and folds
@@ -40,7 +41,7 @@ class CSP(base.BaseEstimator, base.TransformerMixin):
     class_covs = []
 
     # calculate per-class covariance
-    for ci in np.unique(y): 
+    for ci in np.unique(y):
       class_covs.append(np.cov(np.hstack(X[y==ci])))
     assert len(class_covs) == 2
 
@@ -75,4 +76,4 @@ y_pred = pipe.predict(trials[test])
 
 # Show results. Competition results are available on
 # http://www.bbci.de/competition/iii/results/index.html#berlin1
-print metrics.classification_report(y[test], y_pred)
+print(metrics.classification_report(y[test], y_pred))
